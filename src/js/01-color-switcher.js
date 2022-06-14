1fdce6a47a226cacf9b29aaebec4ac5b3178953b
Demian Pashkovskyi <skyangely4@gmail.com>
2022-06-14 22:03:58 +0300

    hw-09 task-01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,21 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const bodyColorEl = document.querySelector('body');
 
+let timerId = null;
+
+
+const getRandomHexColor = () => `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+
+
+startBtn.addEventListener('click', () => {
+	timerId = setInterval(() => {
+		bodyColorEl.style.backgroundColor = getRandomHexColor();
+}, 1000);
+startBtn.disabled = true;
+});
+
+stopBtn.addEventListener('click', () => {
+	clearInterval(timerId);
+	startBtn.disabled = false;
+});
