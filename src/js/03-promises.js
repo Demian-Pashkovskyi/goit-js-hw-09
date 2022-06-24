2378932f9d6d6b7d9fec55851ab34252dfd0c1fa
Demian Pashkovskyi <skyangely4@gmail.com>
2022-06-24 15:35:55 +0300

    виправлення

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -2,36 +2,42 @@ import Notiflix from 'notiflix';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const form = document.querySelector('.form');
+const submitBtn = document.querySelector('button[type="submit"]');
+form.addEventListener('submit', handleSubmit);
 
-function createPromise(position, delay) {
-	return new Promise ((resolve, reject) => {
-		setTimeout(() => {
-			const shouldResolve = Math.random() > 0.3;
-			if (shouldResolve) {
-				resolve({ position, delay });
-			}
-				else {
-					reject({ position, delay });
-				}
-		}, delay);
-	});
-};
-
-form.addEventListener('submit', (event) => {
+function handleSubmit(event) {
 	event.preventDefault();
 	const data = new FormData(form);
 	const delay = parseInt(data.get('delay'));
 	const step = parseInt(data.get('step'));
 	const amount = parseInt(data.get('amount'));
 
-	for (let i = 1; i < amount; i++) {
-		createPromise(i, delay + step * i)
+	setTimeout(() => {
+		submitBtn.removeAttribute('disable');
+	}, delay + amount * step);
+
+	for (let position = 1; position < amount + 1; position++) {
+		createPromise(position, delay)
 		.then(({ position, delay }) => {
-			Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`, { timeout: 4000 });
+			Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
 		})
 		.catch(({ position, delay }) => {
-			Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`, { timeout: 4000 });
-			delay += step;
+			Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
 		});
+		position += step;
 	}
-});
+};
+
+function createPromise(position, delay) {
+	const shouldResolve = Math.random() > 0.3;
+	return new Promise ((resolve, reject) => {
+		setTimeout(() => {
+			if (shouldResolve) {
+				resolve({ position, delay });
+			}	else {
+				reject({ position, delay });
+			}
+		}, delay);
+	});
+};
+
