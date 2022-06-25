b44760dbc046b7919c62d18fd6d5f1593e8bd1c7
Demian Pashkovskyi <skyangely4@gmail.com>
2022-06-25 18:52:47 +0300

    up ver.2.1

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -2,36 +2,37 @@ import Notiflix from 'notiflix';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const form = document.querySelector('.form');
+// const submitBtn = document.querySelector('button[type="submit"]');
 
-
-form.addEventListener('submit', (event) => {
-	event.preventDefault();
-	const data = new FormData(form);
-	const delay = parseInt(data.get('delay'));
-	const step = parseInt(data.get('step'));
-	const amount = parseInt(data.get('amount'));
-
-	for (let i = 1; i < amount + 1; i++) {
-		createPromise(i, delay + step * i)
-		.then(({ position, delay }) => {
-			Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
-		})
-		.catch(({ position, delay }) => {
-			Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
-		});
-	}
-});
+form.addEventListener('submit', handleSabmit);
 
 function createPromise(position, delay) {
+	const shouldResolve = Math.random() > 0.3;
 	return new Promise ((resolve, reject) => {
 		setTimeout(() => {
-			const shouldResolve = Math.random() > 0.3;
 			if (shouldResolve) {
 				resolve({ position, delay });
-			}
-				else {
-					reject({ position, delay });
+			} else {
+				reject({ position, delay });
 				}
 		}, delay);
+		return;
 	});
+};
+
+function handleSabmit(event) {
+	event.preventDefault();
+	const data = event.currentTarget;
+	let delay = Number(data.elements.delay.value);
+	const step = Number(data.elements.step.value);
+	const amount = Number(data.elements.amount.value);
+
+	for (let i = 1; i < amount + 1; i ++) {
+		createPromise(i, delay).then(({ position, delay }) => {
+			Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+		}).catch(({ position, delay }) => {
+			Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+		});
+		delay += step;
+	}
 };
